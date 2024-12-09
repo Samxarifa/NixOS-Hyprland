4f034f3485d7509432aa6bb8b00fa5dca217b0f3
Samxarifa <samuelhay9@gmail.com>
2024-12-09 00:40:17 +0000

    Init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "System Builder Flake with Hyprland";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, ... } @inputs: {
+  
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+
+  };
+}
