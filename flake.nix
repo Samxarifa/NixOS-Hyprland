1c07ef189c05c802170ab0e2b572b03fa66ccc8a
Samxarifa <samuelhay9@gmail.com>
2024-12-10 23:35:48 +0000

    Added (Back) Hardware Config & Removed System from flake

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
   outputs = { nixpkgs, ... } @inputs: {
   
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
